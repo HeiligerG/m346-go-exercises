c85a64410a8bd29fb387997f25f51ebab7b0b8b4
HolyG <gggigi111gmail.com>
2024-11-22 09:18:15 +0100

    go-2-ex-3 abgeschlossen

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[int]int16{
+		104: 10,
+		117: 20,
+		346: 30,
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
 	// TODO: delete one
+	delete(modules, 117)
+
 	// TODO: add one
+	modules[500] = 40
+
 	// TODO: replace one
+	modules[346] = 35
+
 	fmt.Println(modules)
 }
