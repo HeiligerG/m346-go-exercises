b28d99804e8df9c7992c93254fc189c465b18531
HeiligerG <gggigi111@gmail.com>
2024-12-14 12:32:50 +0100

    go-4-ex-2 - no extre ex

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+func computeHypotenuse(a float64, b float64) {
+	aSquare, bSquare := math.Pow(a, 2), math.Pow(b, 2)
+	cHyp := math.Sqrt(aSquare + bSquare)
+	fmt.Println(cHyp)
+}
+
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
 
 func main() {
 	// TODO: call the function computeHypotenuse
+	computeHypotenuse(2.4, 4.1) //4.750789408087881
 }
